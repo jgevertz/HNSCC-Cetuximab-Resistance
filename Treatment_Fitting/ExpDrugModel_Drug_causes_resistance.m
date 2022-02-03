0005a8e0a06fc3e0e39b0ee4faf9b128e496738d
jgevertz <39492566+jgevertz@users.noreply.github.com>
2022-02-03 15:32:59 -0500

    Add files via upload

---
+++ Treatment_Fitting/ExpDrugModel_Drug_causes_resistance.m
@@ -0,0 +1,7 @@
+function xp = ExpDrugModel_Drug_causes_resistance(t,x,r_s,r_r,g,lambda_s,lambda_r,gamma)
+%x(1) is Sensitive cells, x(2) is Resistant cells, x(3) is Drug
+xp = zeros(3,1);
+xp(1) = r_s*x(1)-(g*x(1)*x(3))-(lambda_s)*x(3)*x(1); %sensitive
+xp(2) = r_r*x(2)+(g*x(1)*x(3))-(lambda_r)*x(3)*x(2); %resistant
+xp(3) = -(gamma)*x(3); %drug
+%Volume = xp(1) + xp(2);
